a37461daae7d84ba51ec39d1d43a4518f2be586d
fsu <122@qq.com>
2016-07-22 11:04:05 +0800

    activitycycle

---
+++ app/src/main/java/com/example/f/activitycycle/Activitysecond.java
@@ -0,0 +1,10 @@
+package com.example.f.activitycycle;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lsx on 2016/7/22.
+ */
+//第二个
+public class Activitysecond extends AppCompatActivity {
+}
