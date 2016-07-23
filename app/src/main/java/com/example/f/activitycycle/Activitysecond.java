37e662886701f8d7938301716db21cdd19dd4408
fsu <122@qq.com>
2016-07-23 09:08:41 +0800

    intent 传递数据

--- app/src/main/java/com/example/f/activitycycle/Activitysecond.java
+++ app/src/main/java/com/example/f/activitycycle/Activitysecond.java
@@ -1,10 +1,22 @@
 package com.example.f.activitycycle;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.widget.Toast;
 
 /**
  * Created by lsx on 2016/7/22.
  */
 //第二个
 public class Activitysecond extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        String data = intent.getStringExtra("name");
+        Toast.makeText(Activitysecond.this, data, Toast.LENGTH_SHORT).show();
+    }
 }
--- app/src/main/java/com/example/f/activitycycle/MainActivity.java
+++ app/src/main/java/com/example/f/activitycycle/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -18,7 +19,8 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Activitysecond.class);
+                Intent intent = new Intent(MainActivity.this,Activitysecond.class);
+                intent.putExtra("name","hello");
                 startActivity(intent);
             }
         });
